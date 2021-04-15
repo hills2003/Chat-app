d39a0ea9e8226f82438a459a9ff89ce74a45436a
hillary <75283158+hills2003@users.noreply.github.com>
2021-04-14 22:28:26 -1200

    description

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDom from "react-dom";
+import SimpleTabs from "./Chat-up/SimpleTabs";
+//import Contain from "./whatsapp/contain";
+import Signup from "./Chat-up/Signup";
+import Forgotpassword from "./Chat-up/Forgotpassword";
+import  MainRoute  from "./Chat-up/MainRoute";
+//import Contain from "./whatsapp/Contain";
+ReactDom.render(<MainRoute />,
+    document.getElementById('root'))
